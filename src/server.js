fba45a524e4bc38c4965acb8f04a0a85e79697ca
NikitaIluin <nikiluin63@gmail.com>
2024-09-14 00:33:40 +0300

    commit

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/env.js';
+import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+
+
+
+
+export const setupServer = () => {
+    const app = express();
+    const logger = pino({
+        transport: {
+            target: 'pino-pretty'
+        }
+    });
+
+    app.use(logger);
+    app.use(cors());
+    app.use(express.json());
+
+    app.use(contactsRouter);
+    app.use('*', notFoundHandler);
+    app.use(errorHandler);
+
+
+
+
+    const PORT = Number(env("PORT", 3000));
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+};
